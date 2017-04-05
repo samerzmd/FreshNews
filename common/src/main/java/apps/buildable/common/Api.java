3233c0d49ba959e189ad4f1eed056e7823562b9f
Sam.Z.D <samerzmd@gmail.com>
2017-04-05 23:31:03 +0300

    init

---
+++ common/src/main/java/apps/buildable/common/Api.java
@@ -0,0 +1,8 @@
+package apps.buildable.common;
+
+/**
+ * Created by SamerGigaByte on 4/5/2017.
+ */
+
+public class Api {
+}
---
+++ mobile/src/main/java/apps/buildable/freshnews/MainActivity.java
@@ -0,0 +1,16 @@
+package apps.buildable.freshnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import apps.buildable.common.Api;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
